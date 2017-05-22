340f62048ca66c24c2440ed3f9db68fdcdb1318c
kavish jain <kavishme@gmail.com>
2017-05-21 21:54:31 -0700

    python run

---
+++ run.py
@@ -0,0 +1,2 @@
+import sentiment
+sentiment.run([" ", "/home/simon/workspace/data/sentiment_scores.txt", "/home/simon/workspace/data/dialogs", "1000"])
--- run.sh
+++ run.sh
@@ -2,3 +2,6 @@
 
 echo "Threads, Units, dt, Positives, Negatives"
 ./cmake-build-debug/sentiment ./../data/sentiment_scores.txt ./../data/dialogs/ 100 2>logs/error
+
+# /home/simon/workspace/data/sentiment_scores.txt
+# /home/simon/workspace/data/dialogs
